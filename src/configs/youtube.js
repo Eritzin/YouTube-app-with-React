2fbc092707e0254a57452bb3ea9afb83d4ea99a3
Eri <eritzin@yahoo.co.jp>
2020-01-19 19:58:53 +0100

    deleted API Key

--- src/configs/youtube.js
+++ src/configs/youtube.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const KEY ="AIzaSyBgcpXAM-vNNbWeB3z7f5iQtQ-rxd5Y46Q";
-const KEY ="AIzaSyA3_UcuPQ1RQDOU1pxWy7XYO07ludBbowg";
+
+const KEY ="yourAPIKEY";
 
 export default axios.create({
 baseURL:"https://www.googleapis.com/youtube/v3",
